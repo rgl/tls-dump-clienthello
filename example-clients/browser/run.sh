e8bff28d6e7c8a0bada6d6d50e7dfc89e0325a81
Rui Lopes <rgl@ruilopes.com>
2022-05-31 21:38:45 +0100

    describe where playwright stores the browsers locally

--- example-clients/browser/run.sh
+++ example-clients/browser/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
+# install the dependencies.
+# NB this will install the required browsers automatically.
+#    e.g. at C:\Users\vagrant\AppData\Local\ms-playwright\firefox-1323\firefox\firefox.exe
+# see https://playwright.dev/docs/browsers#managing-browser-binaries
 npm ci
 
+# set the node.js certificate trust store.
 export SSL_CERT_FILE='../../example.com-crt.pem'
 
+# execute the example clients.
 node --use-openssl-ca main.js
